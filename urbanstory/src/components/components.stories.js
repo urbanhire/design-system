38ac4cc935ba1eedfec8d8489c541e8b81479649
Dika Seva <dikaseva@gmail.com>
2019-05-14 16:24:37 +0700

    update styles

--- urbanstory/src/components/components.stories.js
+++ urbanstory/src/components/components.stories.js
@@ -3,12 +3,5 @@ import { storiesOf } from '@storybook/html';
 import button from './button.html';
 
 storiesOf('Components', module)
-	.add('Button', () => button)
-  	.add('Lists', () => {
-	    const button = document.createElement('button');
-	    button.type = 'button';
-	    button.innerText = 'Nanana Button';
-	    button.addEventListener('click', e => console.log(e));
-	    return button;
-  });
+	.add('Button & Links', () => button)
 
--- urbanstory/src/style/style.stories.js
+++ urbanstory/src/style/style.stories.js
@@ -1,7 +1,11 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
 import logo from './logo.html';
+import visuallanguage from './visuallanguage.html';
+import color from './color.html';
 
 storiesOf('Style', module)
+	.add('Color', () => color)
 	.add('Logo', () => logo)
+	.add('Visual Language', () => visuallanguage)
 
