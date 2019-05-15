6f69b326da47eb9c3b4ccb5330d1f1d7e294574a
Dika Seva <dikaseva@gmail.com>
2019-05-15 17:46:33 +0700

    add alert

--- urbanstory/src/components/components.stories.js
+++ urbanstory/src/components/components.stories.js
@@ -2,8 +2,10 @@ import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
 import button from './button.html';
 import avatar from './avatar.html';
+import alert from './alert.html';
 
 storiesOf('Components', module)
+	.add('Alert', () => alert)
 	.add('Avatar', () => avatar)
 	.add('Button & Links', () => button)
 
