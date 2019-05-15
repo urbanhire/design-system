02f8d4d85f90141417352a433475f34fc8b3d1d7
Dika Seva <dikaseva@gmail.com>
2019-05-15 13:43:14 +0700

    add avatar group, and search bar

--- urbanstory/src/cards/cards.stories.js
+++ urbanstory/src/cards/cards.stories.js
@@ -1,7 +1,7 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
-import button from './searchbar.html';
+import searchbar from './searchbar.html';
 
 storiesOf('Cards', module)
-	.add('Search Bars', () => avatar)
+	.add('Search Bars', () => searchbar)
 
