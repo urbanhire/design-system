4e8ba548200f80dcc582c1976db474d3ea760f6d
Dika Seva <dikaseva@gmail.com>
2019-05-15 11:10:26 +0700

    update avatar

---
+++ urbanstory/src/cards/cards.stories.js
@@ -0,0 +1,7 @@
+import { document, console } from 'global';
+import { storiesOf } from '@storybook/html';
+import button from './searchbar.html';
+
+storiesOf('Cards', module)
+	.add('Search Bars', () => avatar)
+
