a9cb90c6d8a49554b5b18dd221ea08a7e00838cc
Dika Seva <dikaseva@gmail.com>
2019-05-20 14:32:57 +0700

    add form input and textarea

--- urbanstory/src/components/components.stories.js
+++ urbanstory/src/components/components.stories.js
@@ -3,9 +3,11 @@ import { storiesOf } from '@storybook/html';
 import button from './button.html';
 import avatar from './avatar.html';
 import alert from './alert.html';
+import toast from './toast.html';
 
 storiesOf('Components', module)
 	.add('Alert', () => alert)
+	.add('Toast', () => toast)
 	.add('Avatar', () => avatar)
 	.add('Button & Links', () => button)
 
