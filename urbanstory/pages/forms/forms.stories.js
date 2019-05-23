c7ccfdd1f47e9724cf3df5b076314324c9088f3f
Dika Seva <dikaseva@gmail.com>
2019-05-23 14:58:00 +0700

    add built files

--- urbanstory/pages/forms/forms.stories.js
+++ urbanstory/pages/forms/forms.stories.js
@@ -2,8 +2,10 @@ import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
 import forms from './form.html';
 import textarea from './textarea.html';
+import select from './select.html';
 
 storiesOf('Forms', module)
-	.add('Select Input', () => forms)
+	.add('Input', () => forms)
 	.add('Textarea', () => textarea)
+	.add('Select', () => select)
 
