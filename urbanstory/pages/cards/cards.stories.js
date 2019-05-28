06c472f8339ba189cec250c12b714cced108adbf
Dika Seva <dikaseva@gmail.com>
2019-05-28 11:13:30 +0700

    delete built

--- urbanstory/pages/cards/cards.stories.js
+++
@@ -1,9 +0,0 @@
-import { document, console } from 'global';
-import { storiesOf } from '@storybook/html';
-import searchbar from './searchbar.html';
-import cardbase from './cardbase.html';
-
-storiesOf('Cards', module)
-	.add('Card Base', () => cardbase)
-	.add('Search Bars', () => searchbar)
-
