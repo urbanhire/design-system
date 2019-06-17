133763333c1684782d96769ba3382ba00630c870
George Matthew Limongan <matthew.limongan@gmail.com>
2019-06-17 17:11:51 +0700

    Added chips page & inserted icon on pagination page

--- urbanstory/pages/components/components.stories.js
+++ urbanstory/pages/components/components.stories.js
@@ -1,27 +1,24 @@
-import {
-	document,
-	console
-} from 'global';
-import {
-	storiesOf
-} from '@storybook/html';
-import button from './button.html';
-import avatar from './avatar.html';
-import alert from './alert.html';
-import toast from './toast.html';
-import tab from './tab.html';
-import labels from './labels.html';
-import list from './list.html';
-import breadcrumbs from './breadcrumbs.html';
-import pagination from './pagination.html';
+import { document, console } from "global";
+import { storiesOf } from "@storybook/html";
+import button from "./button.html";
+import avatar from "./avatar.html";
+import alert from "./alert.html";
+import toast from "./toast.html";
+import tab from "./tab.html";
+import labels from "./labels.html";
+import list from "./list.html";
+import breadcrumbs from "./breadcrumbs.html";
+import pagination from "./pagination.html";
+import chips from "./chips.html";
 
-storiesOf('Components', module)
-	.add('Alert', () => alert)
-	.add('Toast', () => toast)
-	.add('Avatar', () => avatar)
-	.add('Button & Links', () => button)
-	.add('Tab', () => tab)
-	.add('Labels', () => labels)
-	.add('List', () => list)
-	.add('Breadcrumbs', () => breadcrumbs)
-	.add('Pagination', () => pagination)
+storiesOf("Components", module)
+  .add("Alert", () => alert)
+  .add("Toast", () => toast)
+  .add("Avatar", () => avatar)
+  .add("Button & Links", () => button)
+  .add("Tab", () => tab)
+  .add("Labels", () => labels)
+  .add("List", () => list)
+  .add("Breadcrumbs", () => breadcrumbs)
+  .add("Pagination", () => pagination)
+  .add("Chips", () => chips);
