fe6c03fc384f32ca9e02d64e2afad0045736e812
Dika Seva <dikaseva@gmail.com>
2019-07-02 13:05:13 +0700

    add scss pages

--- urbanstory/pages/scss/scss.stories.js
+++ urbanstory/pages/scss/scss.stories.js
@@ -1,7 +1,9 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
 import variables from './variables.html';
+import mixin from './mixin.html';
 
 storiesOf('DEVELOPER|SCSS', module)
 	.add('Variables', () => variables)
+	.add('Mixin', () => mixin)
 
