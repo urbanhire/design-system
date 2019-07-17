4682bdb1c18a4861f9bf267fb39c5c156e0c8682
Dika Seva <dikaseva@gmail.com>
2019-07-17 16:21:11 +0700

    release button 1.0

--- urbanstory/pages/about/about.stories.js
+++ urbanstory/pages/about/about.stories.js
@@ -1,9 +1,11 @@
 import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
 import about from './about.html';
+import gettingstarted from './getting-started.html';
 import css_architecture from './css-architecture.html';
 
 storiesOf('ABOUT|About', module)
 	.add('Urbanstory', () => about)
+	.add('Getting', () => gettingstarted)
 	.add('CSS Architecture', () => css_architecture)
 
