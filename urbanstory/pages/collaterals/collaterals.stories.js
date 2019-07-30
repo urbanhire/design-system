b07083f16bbd7e853b8ae8330ef9a46536f96fee
Dika Seva <dikaseva@gmail.com>
2019-07-30 17:45:35 +0700

    add collaterals

--- urbanstory/pages/collaterals/collaterals.stories.js
+++ urbanstory/pages/collaterals/collaterals.stories.js
@@ -3,9 +3,11 @@ import { storiesOf } from '@storybook/html';
 import productshowcase from './productshowcase.html';
 import corporate from './corporate.html';
 import jobseeker from './jobseeker.html';
+import ats from './ats.html';
 
 storiesOf('GLOBAL|Collaterals', module)
 	.add('Product Showcase', () => productshowcase)
 	.add('Corporate', () => corporate)
 	.add('Jobseeker', () => jobseeker)
+	.add('ATS', () => ats)
 
