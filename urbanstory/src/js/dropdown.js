d85fa4918f4e0742d9f979d788b5a972712bcbe2
nadhifahhanan <nadhifahhnn@gmail.com>
2019-08-08 17:59:17 +0700

    Notification component

--- urbanstory/src/js/dropdown.js
+++ urbanstory/src/js/dropdown.js
@@ -1,12 +1,10 @@
+
 $(document).ready(function () {
-    var dropdown = function() {
-        $('div.has-children').click(function(e) {
-            $(this).addClass('is-active');
-            $(this).children('ul').removeClass('is-hidden');
-        }, function() {
-            $(this).children('ul').addClass('is-hidden');
-            $(this).removeClass('is-active');
-        })
+    var activeIcon = function(){
+    $(".has-children").click(function() {
+        $(this).toggleClass("is-active");
+        });
     }
-    dropdown();
+    activeIcon();
 });
+    
