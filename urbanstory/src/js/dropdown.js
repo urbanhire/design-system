37d079f16efc2bd157e70cadbe239d73b735e1a6
nadhifahhanan <nadhifahhnn@gmail.com>
2019-08-09 18:21:56 +0700

    Working on jqiery for filtering

--- urbanstory/src/js/dropdown.js
+++ urbanstory/src/js/dropdown.js
@@ -1,10 +1,21 @@
 
-$(document).ready(function () {
-    var activeIcon = function(){
-    $(".has-children").click(function() {
-        $(this).toggleClass("is-active");
-        });
+// $(document).ready(function () {
+//     var activeIcon = function(){
+//     $(".has-children").click(function() {
+//         $(this).toggleClass("is-active");
+//         });
+//     }
+//     activeIcon();
+// });
+
+$(document).ready(function(){
+    var lookup = function(){
+    $("#searchInput").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $("#myUL li").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
     }
-    activeIcon();
+    lookup();
 });
-    
