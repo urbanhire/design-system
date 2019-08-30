00b67a3a814c10436caa8dc835feb2127e37c9bf
Dika Seva <dikaseva@gmail.com>
2019-08-30 14:11:30 +0700

    add Blog documentation

--- urbanstory/pages/guidelines/guidelines.stories.js
+++ urbanstory/pages/guidelines/guidelines.stories.js
@@ -2,5 +2,6 @@ import { document, console } from 'global';
 import { storiesOf } from '@storybook/html';
 import blog from './blog.html';
 
+
 storiesOf('GLOBAL|Guidelines', module)
 	.add('Blog', () => blog)
